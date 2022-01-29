87ef95c41ff08e95c49134f391144670a6cea5eb
portareset1 <57542832+portareset1@users.noreply.github.com>
2022-01-29 19:11:49 -0400

    added dynamic fields rendering

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,29 @@
 import "../styles/Form.css";
 import React, { Component } from "react";
 import EditButton from "./EditButton";
-
 class Form extends Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    const { buttonHandler } = this.props;
+    const { id, buttonHandler, formHandler, coords, fields } = this.props;
     return (
-      <form className="Form">
-        <label htmlFor="name">Your name</label>
-        <input className="Form__input" name="name" type="text"></input>
-
-        <label htmlFor="career">Your title</label>
-        <input className="Form__input" name="tile" type="text"></input>
+      <form id={id} style={{ top: coords.y, left: coords.x }} className="Form">
+        {Object.keys(fields).map((field) => {
+          return (
+            <div key={JSON.stringify(field)}>
+              <label htmlFor={field}>
+                {field.charAt(0).toUpperCase() + field.slice(1)}
+              </label>
+              <input
+                onChange={(e) => formHandler(e, id, field)}
+                className="Form__input"
+                name={field}
+              ></input>
+            </div>
+          );
+        })}
 
         <EditButton buttonHandler={buttonHandler} content="Save"></EditButton>
       </form>
